268c3797873942c75a4ee8de127649346e4c90d5
jinchi <jjccha@gmail.com>
2020-05-21 12:45:55 +0800

    Updates

---
+++ precache-manifest.d4bd4537dfba9bde06f7160e2b6d8fe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b98fc04c6576ddc315ad2d111bea98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af29094e99b8c610127e",
+    "url": "/static/css/2.e1bbab56.chunk.css"
+  },
+  {
+    "revision": "2939c4479650cad8bc5b",
+    "url": "/static/css/main.68ebfb9e.chunk.css"
+  },
+  {
+    "revision": "af29094e99b8c610127e",
+    "url": "/static/js/2.17a0a1ed.chunk.js"
+  },
+  {
+    "revision": "80401788b92ac84a88d92de7a6922f70",
+    "url": "/static/js/2.17a0a1ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2939c4479650cad8bc5b",
+    "url": "/static/js/main.2ea38aba.chunk.js"
+  },
+  {
+    "revision": "cf74bfce0915206bcf0a",
+    "url": "/static/js/runtime-main.dad24413.js"
+  }
+]);
