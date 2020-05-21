2bbd325e506b65c0b24fba1f4b1cfafae90e5135
jinchi <jjccha@gmail.com>
2020-05-21 13:58:29 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 
 import Home from './pages/Home/index'
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" component={Home} exact />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
